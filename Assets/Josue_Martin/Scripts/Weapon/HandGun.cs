8ca7f30adba74c20fb7d46b8b6f97a2f8a8cf522
Josue PC home <friki1518@gmail.com>
2024-04-11 00:29:59 -0600

    P1 trabajo en casa

--- Assets/Josue_Martin/Scripts/Weapon/HandGun.cs
+++ Assets/Josue_Martin/Scripts/Weapon/HandGun.cs
@@ -1,31 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 namespace WEAPON
 {
 
     public class HandGun : FireWeapon
     {
+        [Header("General")]
+        [SerializeField] protected TrailRenderer gunLaser;
+        [SerializeField] private Transform raycastOrigin;
+        private RaycastHit hit;
+        [SerializeField] private GameObject bulletPrefabSprite;
+        [SerializeField] private  LayerMask hitMask;
+        
+        [Header("Shoot parametera")]
+        [SerializeField] private float rayDistance = 100;
+        [SerializeField] private float rayForce = 500;
 
-        protected TrailRenderer gunLaser;// Es un laser que muestra a donde apuntas
+        private float lastTimeShoot = Mathf.NegativeInfinity;
+
+        private void Awake() 
+        {
+            damage = 1;
+            actualAmmo = 5;
+            fireRate = 0.5f;
+            maxAmmo = 8;
+            magazineAmmo = 8;
+            reloadTime = 1.5f;
+
+            actualAmmo = maxAmmo;
+
+
+        }
+
 
         internal override void SingleShot() // Disparo con racyast
         {
-            base.SingleShot();
-            Debug.Log("Disparo unico con " + name);
+            if (lastTimeShoot + fireRate < Time.time)
+            {
+                if (actualAmmo >= 1)
+                {
+                    Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hit, rayDistance, hitMask);
+
+                    TrailRenderer trail = Instantiate(gunLaser, raycastOrigin.position, Quaternion.identity);
+
+                    StartCoroutine(SpawnTrail(trail, hit));
+
+                    actualAmmo--;
+
+                }
+            }
         }
 
-        internal override void Reload()
+        private IEnumerator  SpawnTrail (TrailRenderer trail,  RaycastHit hit ) 
         {
-            base.Reload();
-            Debug.Log("Recargando " + name);
+            float time = 0; 
+            Vector3 starPosition = trail.transform.position;
+
+            while (time < 1)
+            {
+                trail.transform.position = Vector3.Lerp(starPosition, hit.point, time);
+                time += Time.deltaTime / trail.time; 
+                yield return null;
+
+
+            }
+
+            trail.transform.position = hit.point ;
+
+            Destroy(trail.gameObject, trail.time);
+            
+
         }
 
-        internal override void Aim() 
+
+
+
+        internal override void Reload() 
         {
-            Debug.Log("Apuntando con " + name);
+            StartCoroutine(WaitingReloading());
+            actualAmmo = actualAmmo + magazineAmmo;
+
+            if (actualAmmo > 8)
+            {
+                actualAmmo = maxAmmo;
+
+            }
+
+        }
+
+        IEnumerator WaitingReloading()
+        {
+            yield return new WaitForSeconds(reloadTime);
+
+        
         }
 
     }
+
+
 }
